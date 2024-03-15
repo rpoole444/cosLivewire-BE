8471f89843af436bbf0948118ede7b7b672a1274
Reid Poole <poole.reid@gmail.com>
2024-03-14 22:12:39 -0600

    add gitignore and knexfile

---
+++ knexfile.js
@@ -0,0 +1,33 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      port: 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
