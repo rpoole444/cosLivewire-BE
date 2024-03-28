7e536a404aa947c534a667726f6b5a49821a2a7f
Reid Poole <poole.reid@gmail.com>
2024-03-28 01:39:59 -0600

    Add new loggedin attriput to user

--- db/migrations/20240315031342_create_users_table.js
+++ db/migrations/20240315031342_create_users_table.js
@@ -6,6 +6,7 @@ exports.up = function(knex) {
     table.string('email').unique().notNullable();
     table.string('password').notNullable();
     table.boolean('is_admin').defaultTo(false);
+    table.boolean('is_logged_in').defaultTo(false);
     table.timestamps(true, true);
   })
 };
