5730eb3b1540b8df5e2b84d3b5b5bf900c4d1fdd
Reid Poole <poole.reid@gmail.com>
2024-04-12 11:55:36 -0600

    Implement Mailer!

--- models/mailer.js
+++ models/mailer.js
@@ -2,21 +2,30 @@
 const nodemailer = require('nodemailer');
 
 // Set up nodemailer with your SMTP details
-const transporter = nodemailer.createTransport({
-  // ...your SMTP configuration...
+let transporter = nodemailer.createTransport({
+  service: 'gmail', // For Gmail, you can use other services or SMTP
+  auth: {
+    user: process.env.EMAIL_USERNAME, // Your email
+    pass: process.env.EMAIL_PASSWORD, // Your email password
+  },
 });
 
-const sendPasswordResetEmail = async (email, link) => {
-  // Logic to send an email
-  const mailOptions = {
-    from: 'youremail@example.com',
-    to: email,
-    subject: 'Password Reset',
-    html: `Password reset link: <a href="${link}">${link}</a>`, // Replace with your email template
-  };
-  await transporter.sendMail(mailOptions);
-};
+const sendPasswordResetEmail = async (email, resetToken) => {
+  const resetPasswordUrl = `http://localhost:3001/resetPasswordRouter/${resetToken}`;
 
+  let mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: email,
+    subject: 'Password Reset Link',
+    html: `<p>Please click on the following link to reset your password:</p><p><a href="${resetPasswordUrl}">${resetPasswordUrl}</a></p>`,
+  };
+  try{    
+    await transporter.sendMail(mailOptions);
+  } catch(e){
+    console.error("Error Sending Email:", e);
+    throw e;
+  }
+}
 module.exports = {
   sendPasswordResetEmail
 };
