906c7c5d8961b144f917632abdecd3b9d56ce938
Reid Poole <poole.reid@gmail.com>
2024-12-04 18:48:22 -0700

    change connection string

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      connectionString: process.env.POSTGRES_URL,
+      connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false }
     },
     migrations: {
