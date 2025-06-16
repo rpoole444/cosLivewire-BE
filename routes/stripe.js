307431897dd3f3c675ea0117d050bfda3f401b31
Reid Poole <111818942+rpoole444@users.noreply.github.com>
2025-06-15 22:12:50 -0600

    refactor: move stripe routes to router

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,10 +1,8 @@
-const environment = process.env.NODE_ENV || 'development';
-const config = require('../knexfile')[environment];
-const knex = require('knex')(config)
 const express = require('express');
 const Stripe = require('stripe');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const knex = require('../db/knex');
 
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
